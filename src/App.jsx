916d26c59c5661b6387c12377d2513c809a5a3c7
Xcel <mystik2002@gmail.com>
2025-07-31 23:41:11 -0400

    update directories

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/classrooms" element={<Classrooms />} />
